2693b1f35116084602ebe042195255a13e49ce76
PerfectAtul <atul@perfectergonomicssystems.in>
2025-05-16 02:27:14 +0530

    comment

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import RootNavigator from 'navigation/RootNavigator';
-import {Provider} from 'react-redux';
-import {store} from './src/redux/store/store';
+// import {Provider} from 'react-redux';
+// import {store} from './src/redux/store/store';
 const App = () => {
   return (
     // <Provider store={store}>
